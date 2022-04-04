791ea537c00adbb889d133b2485e9ae0cf36a11b
kdrake1992 <kdrake1992@outlook.com>
2022-04-04 22:52:34 +0900

    Basic functions now print on screen.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 "use strict"
 
+const buttons = document.querySelectorAll('.buttons');
+const screen = document.querySelector('.screen');
+const clear = document.getElementById('clear');
+
+const clearScreen = function(screen) {
+    screen.innerHTML = '';
+}
+
 const add = function(num1, num2) {
     return num1 + num2;
 }
@@ -27,13 +35,37 @@ const operate = function(num) {
         return(subtract(nums[0],nums[1]))
     }
 
-    else if (num.includes('*')){
-        let nums = num.split('*').map(Number);
+    else if (num.includes('x')){
+        let nums = num.split('x').map(Number);
         return(multiply(nums[0],nums[1]))
     }
 
-    else if (num.includes('/')){
-        let nums = num.split('/').map(Number);
+    else if (num.includes('÷')){
+        let nums = num.split('÷').map(Number);
         return(divide(nums[0],nums[1]))
     }
 }
+
+clearScreen(screen);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', event => {
+
+        if(event.target.innerHTML === '=') {
+            screen.innerHTML = (operate(screen.innerHTML));
+        }
+        else {
+            if(screen.innerHTML.length <= 23) {
+                screen.innerHTML += event.target.innerHTML;
+                console.log(screen.innerHTML.length)
+            }
+            else {
+                screen.innerHTML = "ERROR! NUMBER TOO LARGE!";
+            }
+        }
+    })
+});
+
+clear.addEventListener('click', event => {
+    clearScreen(screen);
+})
