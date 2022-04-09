bbb64b738f158aa6af47974e86942b8a79ab00b7
kdrake1992 <kdrake1992@outlook.com>
2022-04-09 16:34:59 +0900

    Lots of bugs have been fixed, comments added, bugs

--- script.js
+++ script.js
@@ -3,22 +3,35 @@
 const buttons = document.querySelectorAll('.buttons');
 const screen = document.querySelector('.screen');
 const clear = document.getElementById('clear');
+
 let numHolder = new Array(3);
 let answer = 0;
 let hasCalc = 0 ;
+let hasDecimal = false;
 
+// Clears screen and resets attributes.
 const clearScreen = function(screen) {
     screen.innerHTML = '0';
     hasCalc = 0;
     answer = '';
     numHolder = [];
+    hasDecimal = false;
 }
 
-const arrayToString = function(equation) {
-    return equation.join('');
-    
+// Clears attributes not screen.
+const clearAttributes = function() {
+    hasCalc = 0;
+    answer = '';
+    numHolder = [];
+    hasDecimal = false;
 }
 
+// Rounds number if too big
+const rounder = function(answer) {
+    return Number(Math.round(answer + "e" + 23) + "e-" + 23); 
+}
+
+// Basic arithmetic functions, takes in 2 numbers.
 const add = function(num1, num2) {
     return num1 + num2;
 }
@@ -35,6 +48,8 @@ const divide = function(num1, num2) {
     return num1 / num2;
 }
 
+// Operator, takes in an array, joins the arary into a string 
+// and then sends a string to arithmetic functions which it returns
 const operate = function(equation) {
     let num = equation.join('');
     console.log(num);
@@ -56,29 +71,52 @@ const operate = function(equation) {
 
     else if (num.includes('÷')){
         let nums = num.split('÷').map(Number);
-        return(divide(nums[0],nums[1]))
+        if(nums[0] === 0 || nums[1] === 0) {
+            return 'ERROR! CANNOT DIVIDE 0!'
+        }
+        else {
+            return(divide(nums[0],nums[1]))
+        }
     }
 }
 
+// Intial clears screen
 clearScreen(screen);
 
 
+// Button listeners
 buttons.forEach((button) => {
 
     button.addEventListener('click', event => {
 
+        // Checks to see if user presses =
         if(event.target.innerHTML === '=') {
             if(numHolder.length == 2) {
                 numHolder[2] = answer;
-                screen.innerHTML = (operate(numHolder));
+
+                if(numHolder[2] === '') {
+                    console.log('hello');
+                    screen.innerHTML = 'ERROR';
+                    clearAttributes();
+                }
+                else {
+                    screen.innerHTML = (operate(numHolder));
+
+                }
+
             }
             else {
                 screen.innerHTML = 'ERROR';
+                clearAttributes();
             }
 
 
         }
+
         else {
+
+            // If there are over 23 characters, itll say error
+            // will fix later to rounding
             if(screen.innerHTML.length <= 23) {
                 if(screen.innerHTML === '0') {
                     answer = event.target.innerHTML;
@@ -87,6 +125,8 @@ buttons.forEach((button) => {
 
                 }
                 else {
+                    // if the user uses an operator it gets ready to
+                    // calculate
                     if(event.target.innerHTML === '+' || 
                         event.target.innerHTML === '-' || 
                         event.target.innerHTML === 'x' || 
@@ -96,23 +136,37 @@ buttons.forEach((button) => {
                             numHolder[0] = answer;
                             numHolder[1] = event.target.innerHTML;
                             answer = '';
+                            hasDecimal = false;
                             hasCalc++;
+
                         }
 
-
+                        // operates the previous two numbers
+                        // allows for multiple operations
                         else if(hasCalc == 1) {
                             numHolder[2] = answer;
                             screen.innerHTML = (operate(numHolder));
                             numHolder[0] = screen.innerHTML;
                             numHolder[1] = event.target.innerHTML;
                             answer = '';
+                            hasDecimal = false;
                         }
                     }
-
+                    
+                    // attaches number to the string and checks demicals
                     else {
-                        answer += event.target.innerHTML;
-                        screen.innerHTML = answer;
-
+                        if(event.target.innerHTML === '.' && hasDecimal == false ) {
+                            answer += event.target.innerHTML;
+                            screen.innerHTML = answer;
+                            hasDecimal = true;
+                        }
+                        else if(event.target.innerHTML === '.' && hasDecimal == true) {
+                            //DO nothing if already one decimal
+                        }
+                        else {
+                            answer += event.target.innerHTML;
+                            screen.innerHTML = answer;
+                        }
                     }
 
                 }
@@ -124,6 +178,7 @@ buttons.forEach((button) => {
     })
 });
 
+// clears screen once the clear button is pressed
 clear.addEventListener('click', event => {
     clearScreen(screen);
 })
